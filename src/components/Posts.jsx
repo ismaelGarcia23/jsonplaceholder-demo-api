8bbfeb8506cc7393f161264deb7b73a03e903fd0
KeniaKodigo <kenia.paz@kodigo.org>
2025-07-02 21:01:45 -0600

    consumiendo endpoint con fetch y axios

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { getPosts } from '../services/postServices';
 
 const Container = styled.section`
     margin: 2.5rem 5rem;
@@ -13,10 +14,39 @@ const Title = styled.h1`
 `
 
 export default function Posts() {
+    //declarando un estado para la lista de posts de la api
+    const [posts, setPosts] = useState([])
+
+    //metodo que recibe la informacion de la api (posts)
+    const fetchData = async () => {
+        const data = await getPosts();
+        //devuelve el arreglo de los posts que hay en la api
+        console.log(data)
+        setPosts(data)
+    }
+
+    //hook => useEffect
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
         <Container>
             <Title>Latest Posts</Title>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, nostrum!</p>
+            <section>
+                {
+                    posts.map((item) => {
+                        return (
+                            <div key={item.id} style={{borderBottom: "1px solid blue", paddingBottom: ".2rem"}}>
+                                <h4>{item.title}</h4>
+                                <p>{item.body}</p>
+                                <p><b>userId: </b>{item.userId}</p>
+                            </div>
+                        )
+                    })
+                }
+            </section>
         </Container>
     )
 }
--- src/services/postServices.js
+++ src/services/postServices.js
@@ -1,1 +1,35 @@
+import axios from "axios";
+
 //aqui se colocara las peticiones de la api (solo logica)
+
+//metodo que me devuelva los posts de la api
+const getPosts = async () => {
+    try{
+        //colocar la peticion HTTP (get, post, put, patch, delete)
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+        //la respuesta ya se convierte en un json
+        console.log(response);
+        return response.data; //data => representa la informacion del endpoint
+    }catch(error){
+        console.error("error al obtener los datos", error);
+    }
+}
+
+export { getPosts }
+
+// const obtenerPublicaciones = async () => {
+//     try{
+//         const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//             method: "PUT",
+//             data: {
+
+//             }
+//         }) //get
+//         //convierto la respuesta en un json
+//         const lista_post = response.json()
+//         console.log(lista_post); //[]
+        
+//     }catch(error){
+//         console.error("Error al obtener los post", error)
+//     }
+// }
